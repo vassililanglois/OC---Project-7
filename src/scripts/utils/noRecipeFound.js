4c7706c3705113253cb8574360cf8e99adfaf91a
Vassili <vassililanglois@icloud.com>
2025-05-08 16:32:09 +0200

    fix(noRecipeFound.js):little correction of textcontent

--- src/scripts/utils/noRecipeFound.js
+++ src/scripts/utils/noRecipeFound.js
@@ -3,12 +3,16 @@ const errorMessage = document.querySelector(".error-message");
 export function displayErrorMessage(keyword) {
   errorMessage.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-      <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 15H13V17H11V15ZM11 7H13V13H11V7Z"></path>
+      <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 
+      2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 
+      16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183
+       7.58172 20 12 20ZM11 15H13V17H11V15ZM11 7H13V13H11V7Z"></path>
     </svg>
   `;
 
   const message = document.createElement("p");
-  message.textContent = `Aucune recette ne contient «${keyword}» vous pouvez chercher « tarte aux pommes », « poisson », etc.`;
+  message.textContent = `Aucune recette ne contient «${keyword}» vous pouvez 
+  chercher « tarte aux pommes », « poisson », etc.`;
 
   errorMessage.appendChild(message);
   errorMessage.style.display = "flex";
